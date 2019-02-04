2db98f95fbddf28da29b7fc0e9c993527b9d4d6f
Masahiro Harada <masahiro.harada@ari-jp.com>
2019-02-04 12:27:49 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Vue.js でモーダルウィンドウを作ってみよう
+
+## 課題
+
+後述する仕様の通りに動作するように `app.js` を編集しましょう。  
+穴埋めになっているので `Insert code here...` の箇所にコードを足してください。
+
+HTML と CSS は出来ているので変更しません。  
+ただし重要なヒントですのでよく確認してください。
+
+## 仕様
+
+- Open ボタンをクリックするとモーダルが開かれます。
+- モーダルが開かれた状態でオーバーレイ（背景の黒い部分）またはバツ印をクリックすると、モーダルが閉じられます。
+
+## キーワード
+
+- `v-model` による親子コンポーネント間のコミュニケーション
+- `slot` によるコンテンツの配信
+
+## 提出方法
+
+`solution/{your name}` のルールでブランチを作成してください。
+
+```console
+$ git clone git@github.com:bis-study-group/vuejs-photo-gallery.git
+$ git checkout -b solution/name
+```
+
+解き終わったらリモートリポジトリにプッシュしてください。
+
+```console
+$ git push origin solution/name
+```
---
+++ app.js
@@ -0,0 +1,44 @@
+Vue.component('modal', {
+  template: `
+    <div class="modal" :class="cssClass">
+      <div class="modal-overlay" @click="close">
+        <div class="modal-container">
+          <header class="modal-header">
+            <h2 class="modal-title">
+              <!-- Insert code here... -->
+            </h2>
+            <button class="modal-close" @click="close">&times;</button>
+          </header>
+          <main class="modal-content">
+            <!-- Insert code here... -->
+          </main>
+        </div>
+      </div>
+    </div>
+  `,
+  props: [
+    /* Insert code here... */
+  ],
+  computed: {
+    cssClass() {
+      /* Insert code here... */
+    },
+  },
+  methods: {
+    close() {
+      /* Insert code here... */
+    },
+  },
+});
+
+new Vue({
+  el: '#app',
+  data: {
+    modalOpen: false,
+  },
+  methods: {
+    open() {
+      /* Insert code here... */
+    },
+  },
+});
